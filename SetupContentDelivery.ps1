343fddee7eb6cd27eb6a4a9a93c721ca691a11bc
Dominic Cronin <dominic@cronin.tech>
2017-08-20 18:50:59 +0200

    Just getting started

---
+++ SetupContentDelivery.ps1
@@ -0,0 +1,34 @@
+﻿#Parking this effort for now while I look at the quickinstall script
+
+param(
+[ValidateScript({Test-Path $_ -PathType 'Container'})] 
+[string]$InstallerDirectoryPath='C:\Users\NetAdmin\Downloads\SDL Web 8.5',
+
+[string]$ServicesDirectoryPath='C:\SDLServices',
+
+[string[]]$SitePrefixes=("Staging","Live")
+
+)
+
+# ValidateScript won't check the default value, so do it here
+if (-not (Test-Path ($InstallerDirectoryPath + '\Content Delivery'))) {
+    throw "That doesn't look like the installation directory: bailing...."
+}
+
+if (-not (Test-path $ServicesDirectoryPath) ){
+    $ServicesDirectory = New-Item -ItemType Directory -Path $ServicesDirectoryPath
+}
+else {
+    $ServicesDirectory = Get-Item $ServicesDirectoryPath
+}
+set-location $ServicesDirectory
+
+#Disco
+$SitePrefixes | % {Copy-Item -Recurse "C:\Users\NetAdmin\Downloads\SDL Web 8.5\Content Delivery\roles\discovery\standalone" ($_ + "Discovery")}  
+
+
+
+
+
+
+
