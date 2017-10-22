7f6433df1e421af37231db3fc9e2d17a2af81a2d
Dominic Cronin <dominic@cronin.tech>
2017-10-22 18:48:57 +0200

    Fixed URLs for model service

--- SetupContentDelivery.ps1
+++ SetupContentDelivery.ps1
@@ -308,7 +308,7 @@ Copy-Item -Recurse "$DXAInstallerDirectoryPath\cis\dxa-model-service\standalone"
 $discoveryStorageConfig = (resolve-path ("Discovery\config\cd_storage_conf.xml"))
 & "$ScriptPath\Merge-RoleToConfigRepository.ps1" -storageConfig $discoveryStorageConfig `
                                                  -roleName 'ContentServiceCapability' `
-                                                 -roleProperties @{'dxa-model-service'="http://${$domainName}:8998"}
+                                                 -roleProperties @{'dxa-model-service'="http://$($domainName):8998"}
 
 & "$ScriptPath\Merge-Logback.ps1" -logbackFile (resolve-path ("DxaModel\config\logback.xml")) `
                                   -logFolder "$LoggingOutputPath\DxaModel"
@@ -330,7 +330,7 @@ Copy-Item -Recurse "$DXAInstallerDirectoryPath\cis\dxa-model-service\standalone"
 $stagingDiscoveryStorageConfig = (resolve-path ("StagingDiscovery\config\cd_storage_conf.xml"))
 & "$ScriptPath\Merge-RoleToConfigRepository.ps1" -storageConfig $stagingDiscoveryStorageConfig `
                                                  -roleName 'ContentServiceCapability' `
-                                                 -roleProperties @{'dxa-model-service'="http://${$domainName}:9998"}
+                                                 -roleProperties @{'dxa-model-service'="http://$($domainName):9998"}
 
 
 & "$ScriptPath\Merge-Logback.ps1" -logbackFile (resolve-path ("StagingDxaModel\config\logback.xml")) `
